53a6cd5e8d88973ef48cc0a9c2f3b2f7bba13511
Dadan Juanda <kang.dj749@gmail.com>
2025-08-02 05:56:52 +0700

    Fix Error Route to GSheet

--- src/app/api/submit/route.ts
+++ src/app/api/submit/route.ts
@@ -24,7 +24,7 @@ export async function POST(req: NextRequest) {
   const auth = new google.auth.GoogleAuth({
     credentials: {
       client_email: process.env.GS_CLIENT_EMAIL,
-      private_key: process.env.GS_PRIVATE_KEY?.replace(/\n/g, '\n'),
+      private_key: process.env.GS_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     },
     scopes: ['https://www.googleapis.com/auth/spreadsheets'],
   });
